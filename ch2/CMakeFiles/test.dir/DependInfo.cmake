e86c92f35fe35b9160da2ff089956850ce0ca754
Steven <na>
2014-09-06 15:52:33 -0500

    first clean version

---
+++ ch2/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/stevenw/Code/C++/TDD/Langr_s_book/code/c2/1/SoundexTest.cpp" "/Users/stevenw/Code/C++/TDD/Langr_s_book/code/c2/1/CMakeFiles/test.dir/SoundexTest.cpp.o"
+  "/Users/stevenw/Code/C++/TDD/Langr_s_book/code/c2/1/main.cpp" "/Users/stevenw/Code/C++/TDD/Langr_s_book/code/c2/1/CMakeFiles/test.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Users/stevenw/Install/gmock/1.6/include"
+  "/Users/stevenw/Install/gmock/1.6/gtest/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
